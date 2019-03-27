580ff4d8bc828190bbae9867e9ef22c32f3f05ec
root <s.rau@gridx.de>
2019-03-27 17:38:56 +0000

    First running version

--- Makefile
+++ Makefile
@@ -3,6 +3,8 @@ all: bin/hm-demo bin/hm-demo-arm.linux
 
 bin: 
 	mkdir -p bin
+now: bin
+	CGO_ENABLED=0 go build -o bin/hm-demo ./cmd/hm-demo
 
 bin/hm-demo: bin
 	go build -o bin/hm-demo ./cmd/hm-demo
--- cmd/hm-demo/main.go
+++ cmd/hm-demo/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/binary"
 	"fmt"
+	"time"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 	log "github.com/sirupsen/logrus"
@@ -17,12 +18,12 @@ func main() {
 	var (
 		logger = log.New()
 
-		mqttBroker = flag.String("mqtt.broker", "tcp://127.0.0.1:1883", "Broker to connect to")
+		mqttBroker = flag.String("mqtt.broker", "tcp://192.168.192.107:1883", "Broker to connect to")
 
-		spiDev      = flag.String("spi.dev", "SPI0.0", "SPI port")
+		spiDev      = flag.String("spi.dev", "SPI1.0", "SPI port")
 		spiMode     = flag.Int("spi.mode", 0x0, "Communication Mode")
 		spiBits     = flag.Int("spi.bits", 8, "Number of bits per word")
-		spiMaxSpeed = flag.Int("spi.max-speed", 2800000, "Maximum rated speed by the device's spec in µHz")
+		spiMaxSpeed = flag.Int64("spi.max-speed", 28000000000, "Maximum rated speed by the device's spec in µHz")
 	)
 	flag.Parse()
 
@@ -33,6 +34,7 @@ func main() {
 	opts.AddBroker(*mqttBroker)
 	client := mqtt.NewClient(opts)
 	// Establish a mqtt connection.
+
 	if token := client.Connect(); token.Wait() && token.Error() != nil {
 		logger.WithFields(mqttLogFields).Fatalf("mqtt.Connect: %+v", token.Error())
 	}
@@ -59,21 +61,20 @@ func main() {
 		logger.WithFields(spiLogFields).Fatalf("spireg.Open: %+v", err)
 	}
 
-	/*
-		// For testing only
-		port := spitest.Record{
-			Port: &spitest.Playback{
-				Playback: conntest.Playback{
-					Ops: []conntest.IO{
-						{W: nil, R: []byte{'a', 'b', 'c', 0, 0, 4, 0, 0}},
-						{W: nil, R: []byte{'a', 'b', 'd', 0, 150, 142, 202, 1}},
-					},
-					DontPanic: true,
-				},
-			},
-		}
-	*/
 
+	// For testing only
+/*	port := spitest.Record{
+		Port: &spitest.Playback{
+			Playback: conntest.Playback{
+				Ops: []conntest.IO{
+					{W: nil, R: []byte{'a', 'b', 'c', 0, 0, 4, 0, 0}},
+					{W: nil, R: []byte{'a', 'b', 'd', 0, 150, 142, 202, 1}},
+				},
+				DontPanic: true,
+			},
+		},
+	}
+*/
 	// Close port when program exists.
 	defer port.Close()
 
@@ -87,60 +88,107 @@ func main() {
 	messages := make(chan Message)
 
 	go func() {
-		buf := make([]byte, 8) //  8 Bytes are read at once.
+		buf := make([]byte, 11) //  11 Bytes are read at once.
 		for {
 			err := conn.Tx(nil, buf[:])
 			if err != nil {
 				logger.WithFields(spiLogFields).Errorf("spi.Tx: %+v", err)
 				close(messages)
 				return
+
 			}
+			//logger.WithFields(spiLogFields).Infof("Bytes: %#X", buf[:])
 			msg, err := NewMessage(buf[:])
+
+//			logger.WithFields(spiLogFields).Infof("StatusFlags:\t\t %X",	msg.StatusFlags)
+//			logger.WithFields(spiLogFields).Infof("ErrorCode:\t\t %d",	msg.ErrorCode)
+//			logger.WithFields(spiLogFields).Infof("PlcID:\t\t %d",		msg.PlcID)
+//			logger.WithFields(spiLogFields).Infof("Freq.:\t\t %d", 	msg.Freq)
+//			logger.WithFields(spiLogFields).Infof("Counter:\t\t %d", 	msg.CounterValue)
+//			logger.WithFields(spiLogFields).Infof("CRC:\t\t %d", 		msg.Crc16)
+
 			if err != nil {
 				logger.WithFields(spiLogFields).Fatalf("NewMessage: %+v", err)
 				continue
 			}
+
+			//logger.WithFields(spiLogFields).Infof("Bytes: %q", buf[:])
 			messages <- msg // Put parsed message into channel.
+			time.Sleep(250 * time.Millisecond)
 		}
 	}()
 
 	for msg := range messages { // Read from channel.
-		logger.WithFields(mqttLogFields).WithField("mqtt.topic", msg.Topic).
-			Infof("Send message: %s: %+v", msg.Topic, msg.Bytes())
+		//logger.WithFields(mqttLogFields).WithField("mqtt.topic", msg.Topic).Infof("Send message: %s: %+v", msg.Topic, msg.Bytes()
 
-		token := client.Publish(msg.Topic, 0x0, false, msg.Bytes())
+		topic := "sensors/gridBox/rpm"
+		if msg.Freq == 32776{
+			msg.Freq = 0
+		}
+		token := client.Publish(topic, 0x0, false, []byte(fmt.Sprintf("%d", msg.Freq)))
 		token.Wait()
 		if token.Error() != nil {
-			logger.WithFields(mqttLogFields).WithField("mqtt.topic", msg.Topic).
-				Errorf("mqtt.Publish(%s, %q): %+v", msg.Topic, msg.Bytes(), token.Error())
+			logger.WithFields(mqttLogFields).WithField("mqtt.topic", topic).
+				Errorf("mqtt.Publish(%s, %x: %+v", topic, msg.Freq, token.Error())
 		}
+
+		topic = "sensors/gridBox/plcId"
+		token = client.Publish(topic, 0x0, false, []byte(fmt.Sprintf("%d", msg.PlcID)))
+                token.Wait()
+                if token.Error() != nil {
+                        logger.WithFields(mqttLogFields).WithField("mqtt.topic", topic).
+                                Errorf("mqtt.Publish(%s, %x: %+v", topic, msg.PlcID, token.Error())
+                }
+		topic = "sensors/gridBox/count"
+                token = client.Publish(topic, 0x0, false, []byte(fmt.Sprintf("%d", msg.CounterValue)))
+                token.Wait()
+                if token.Error() != nil {
+                        logger.WithFields(mqttLogFields).WithField("mqtt.topic", topic).
+                                Errorf("mqtt.Publish(%s, %x: %+v", topic, msg.CounterValue, token.Error())
+                }
+
+		topic = "sensors/gridBox/status"
+                token = client.Publish(topic, 0x0, false, []byte(fmt.Sprintf("%d", msg.StatusFlags)))
+                token.Wait()
+                if token.Error() != nil {
+                        logger.WithFields(mqttLogFields).WithField("mqtt.topic", topic).
+                                Errorf("mqtt.Publish(%s, %x: %+v", topic, msg.CounterValue, token.Error())
+                }
 	}
 }
 
 // Message represents an mqtt message.
 type Message struct {
-	Topic string
-	//  TODO(s.rau): Adjust the number of fields.
-	Value int
+	StatusFlags	int;
+	ErrorCode	int;
+	PlcID		int;
+	Freq		int;
+	CounterValue	int;
+	Crc16		int;
 }
 
 // NewMessage converts SPI bytes into a message struct.
 func NewMessage(d []byte) (Message, error) {
-	if len(d) != 8 {
-		return Message{}, fmt.Errorf("NewMessage: mismatch number of bytes %d != 8", len(d))
+	if len(d) != 11 {
+		return Message{}, fmt.Errorf("NewMessage: mismatch number of bytes %d != 11", len(d))
 	}
 
 	return Message{
-		Topic: string(d[0:3]),
-		// TODO (s.rau): convert the SPI read to actual values.
-		Value: int(binary.LittleEndian.Uint32(d[4:])),
+		//d[0] empty Byte, should be 0xAA
+		StatusFlags	: int(d[1]),
+		ErrorCode	: int(d[2]),
+		PlcID		: int(binary.LittleEndian.Uint16(d[3:5])),
+		Freq		: int(binary.LittleEndian.Uint16(d[5:7])),
+		CounterValue    : int(binary.LittleEndian.Uint16(d[7:9])),
+//		Crc16		: int(binary.LittleEndian.Uint16(d[9:11])),
 	}, nil
+
 }
 
 // Bytes returns a byte representation of the value(s).
 func (m Message) Bytes() []byte {
-	// For JSON output, remove comment
+// For JSON output, remove comment
 	//d, _ := json.Marshal(m)
 	//return d
-	return []byte(fmt.Sprintf("%d", m.Value))
+	return []byte(fmt.Sprintf("%#X", m.PlcID))
 }
